38d64562350a737e067b51b612c688a5f352f573
ImperioKDB <kingbrite123@gmail.com>
2026-06-11 22:39:02 +0100

    restore: src/middleware.js to v11

--- src/middleware.js
+++ src/middleware.js
@@ -37,7 +37,6 @@ export async function middleware(request) {
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
     !request.nextUrl.pathname.startsWith('/signup') &&
-    !request.nextUrl.pathname.startsWith('/confirm-email') &&
     request.nextUrl.pathname !== '/'
   ) {
     const url = request.nextUrl.clone()
