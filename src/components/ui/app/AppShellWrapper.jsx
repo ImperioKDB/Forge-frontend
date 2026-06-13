fb4aeac6fbac51f2569112747798c59632bb1688
ImperioKDB <kingbrite123@gmail.com>
2026-06-13 01:12:54 +0100

    fix: correct useKeyboardShortcuts import path in AppShellWrapper

--- src/components/ui/app/AppShellWrapper.jsx
+++ src/components/ui/app/AppShellWrapper.jsx
@@ -8,7 +8,7 @@
 import AppShell from './AppShell'
 import ErrorBoundary from '../ErrorBoundary'
 import { ToastProvider } from '../Toast'
-import { useKeyboardShortcuts, ShortcutHelp } from '../../hooks/useKeyboardShortcuts'
+import { useKeyboardShortcuts, ShortcutHelp } from '../../../lib/hooks/useKeyboardShortcuts'
 import { useState } from 'react'
 
 export default function AppShellWrapper({ children }) {
